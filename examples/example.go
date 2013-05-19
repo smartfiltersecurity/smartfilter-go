ef11c87e1170dbe3d50fbaf50810e513b8bdf5e6
Kunal Anand <kunal.r.anand@gmail.com>
2013-05-18 22:21:17 -0700

    Added example.

---
+++ examples/example.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prevoty/smartfilter-go/client"
+)
+
+func main() {
+	apiKey := "api key goes here"
+	ruleKey := "rule key goes here"
+	input := "the <script>alert('quick brown fox');</script> jumps over the lazy dog & mouse"
+
+	// Create an instance of SmartFilter
+	sf := client.NewSmartFilterClient(apiKey)
+
+	// Verify the API key
+	verified, verifiedErr := sf.Verify()
+	if verified {
+		// Get API key information
+		info, infoErr := sf.Info()
+		if infoErr == nil {
+			fmt.Println("Information:", info.Message)
+			// Verify rule
+			verification, verifyErr := sf.VerifyRule(ruleKey)
+			fmt.Println("Verified rule:", verification, verifyErr)
+			// Filter XSS
+			result, filterErr := sf.Filter(input, ruleKey)
+			fmt.Println("Filtered output:", result.Output, filterErr)
+		} else {
+			fmt.Println("Could not get information")
+		}
+	} else {
+		fmt.Println("API key not verified", verifiedErr)
+	}
+}
